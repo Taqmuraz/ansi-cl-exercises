4d22f4c3d8df796bd46e14a7c9b99c48a5a21b5e
lulook <lulook23@gmail.com>
2024-12-25 01:09:49 +0100

    quarter-turn

---
+++ chapter-two.lisp
@@ -0,0 +1,27 @@
+(defun quarter-turn (a)
+  (let* (
+      (dims (array-dimensions a))
+      (w (first dims))
+      (h (second dims))
+      (cols (loop for x below w collect
+        (loop for y below h collect
+          (aref a x y)
+        )
+      ))
+      (r (make-array (list h w)))
+      (trans (apply #'mapcar #'list cols))
+      (new-cols (mapcar #'reverse trans))
+      (x 0)
+      (y 0)
+    )
+    (loop for c in new-cols do
+      (loop for i in c do
+        (setf (aref r y x) i)
+        (incf x)
+      )
+      (setf x 0)
+      (incf y)
+    )
+    r
+  )
+)
