3448ea045b6f20faf73bd841dc0f65625ffe4cb7
lulook <lulook23@gmail.com>
2024-12-27 15:14:18 +0100

    precedes-rec

--- chapter-five.lisp
+++ chapter-five.lisp
@@ -3,11 +3,20 @@
       (r nil)
       (l (coerce seq 'list))
     )
-    (dolist (c (maplist #'identity l)) (format t "~A~%" c)
+    (dolist (c (maplist #'identity l))
       (if (and (< 1 (length c)) (eql e (second c)))
         (setf r (append r (list (first c))))
       )
     )
     r
+  )
+)
+
+(defun precedes-rec (e seq)
+  (if (and (< 1 (length seq)))
+    (append
+      (if (eql e (elt seq 1)) (list (elt seq 0)))
+      (precedes-rec e (subseq seq 1))
+    )
   )
 )
